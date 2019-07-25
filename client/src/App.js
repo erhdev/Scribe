5ed6809b20fd2fe627a18dd1a5a6d8364480d02f
madflowerr <emerson.harmon@gmail.com>
2019-07-25 17:28:00 -0400

    pivoting to basic backend confirmation for time reasons

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './css/App.css';
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <Route exact path="/" component={Login} />  
+      <Route exact path="/home" component={Dashboard} />
+      </div>
+    </Router>
   );
 }
 
